01470338d3c9108fdb649f9ee1afed9cef8b8204
corwin <42521188+corwinperdomo@users.noreply.github.com>
2018-12-24 12:29:20 -0800

    fixed broken code from last commit

--- weather.py
+++ weather.py
@@ -4,9 +4,8 @@ import json
 api_key = ""
 cords = "34.0575651,-117.820741"
 url = "https://api.darksky.net/forecast/{}".format(api_key)+"/{}".format(cords)
-
-
 json_data = requests.get(url).json()
+
 
 # pull current and get current temp
 get_all = json_data['currently']
@@ -25,7 +24,7 @@ print('Current Weather at Cal Poly Pomona: ')
 print('Temp:', int(temp), '˚F')
 print('High:', int(tempHigh), '˚F')
 print("Low: ", int(tempLow), '˚F"')
-print('Chance of Rain: {:d}{}'.format(precip, '%'))
+print("Chance of Rain: ", int(precip), "%")
 print(summary)
 
 #print(get_daily_data)
